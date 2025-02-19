8c6e82334c56933c65a9fa5f66c288b3d12b249c
“[firstname <josiahrbernard@gmail.com>
2025-02-19 11:03:18 -0500

    Refactored movement_script into a class and initialized story_script"

--- scripts/movement_script.py
+++ scripts/movement_script.py
@@ -6,124 +6,67 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import os
 
-# Initialize ROS Node
-rospy.init_node('emotion_based_movement')
 
-cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-rospy.sleep(1)  
+class Turtlebot:
+    def __init__(self):
+        # Initialize ROS Node
+        rospy.init_node('emotion_based_movement')
 
+        cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        rospy.sleep(1)  
 
+        #Array of emotions movesments (duration,linar_velocity, angular_velocityt)
 
-def autonomousMovement(name, duration, linear_vel, angular_vel):
+        self.emotion = {
+            "joyful" : [( 2, 0.4, 2), (2, 0.4, 2), (1, 0.6, 0)],
+            "sad" : [(1.5,-0.2,0.55),(1.5,-0.2,-0.6)],
+            "anger":[( 5, 0.1, -1.0)],
+            "fear": [( 1.5, -0.7, -0.6)],
+            "confident":[(2, 0.3, 0.3),(1.25, 0.2, 0.9),(1.25, 0.2, -0.9),(1, 0.5, 0)],
+            "happy": [(5, 0.4, -1.5)],
+            "frustrated":[(5, 0.4, 1.5)],
+            "tired":[(5, 0.1, 0)],
+            "neutral":[],
+        }
 
-    move_cmd = Twist()
-    move_cmd.linear.x = linear_vel
-    move_cmd.angular.z = angular_vel
+        
+    def autonomousMovement(duration, linear_vel, angular_vel):
 
-    start_time = rospy.get_time()
-    rate = rospy.Rate(10)
+        move_cmd = Twist()
+        move_cmd.linear.x = linear_vel
+        move_cmd.angular.z = angular_vel
 
-    while not rospy.is_shutdown() and (rospy.get_time() - start_time) < duration:
+        start_time = rospy.get_time()
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown() and (rospy.get_time() - start_time) < duration:
+            cmd_vel_pub.publish(move_cmd)
+            rate.sleep()
+
+        # Stop the robot
+        move_cmd.linear.x = 0.0
+        move_cmd.angular.z = 0.0
         cmd_vel_pub.publish(move_cmd)
-        rate.sleep()
-
-    # Stop the robot
-    move_cmd.linear.x = 0.0
-    move_cmd.angular.z = 0.0
-    cmd_vel_pub.publish(move_cmd)
-    rospy.loginfo(f"'{name}' movement completed.")
-
+        # rospy.loginfo(f"'{name}' movement completed.")
+    
+    def executeEmotion(self,emotion):
+        if emotion in self.emotion:
+            rospy.loginfo(f"Executing {emotion} script")
+            for duration,linear_vel,angular_vel in self.emotions[emotion]:
+                autonomousMovement(duration,linear_vel,angular_vel)
+        else:
+            rospy.loginfo("Unknown emotion entered. Please try again")
 
 
 def main():
-
+    bot = Turtlebot()
     while not rospy.is_shutdown():
-        #Joyful,Sad,Anger,Fear,Confident,Happy,Frustrated,Tired,Neutral
-        emotions = ["joyful","sad","anger","fear","confident","happy","frustrated","tired","neutral"]
-        print("Availiable Emotions: ",emotions)
-        emotion = input("Enter an emotion: ")
-
-    
-        
-        if emotion in emotions:
-            rospy.loginfo(f"Executing '{emotion}' movement...")
-        
-        else:
-            rospy.loginfo("Unknown emotion entered. Please try again.")
-            continue
-
-        #Emotion Movements
-
-        #Joyful-Movement
-        if emotion == "joyful":
-            try:
-                for _ in range(2):  # Repeat the joyful spin multiple times
-                    autonomousMovement('joyful_script', 2, 0.4, 2)  
-                autonomousMovement('joyful_script', 1, 0.6, 0)
-                
-            except rospy.ROSInterruptException:
-                pass
-
-        #Sad-Movement
-        elif emotion == "sad":
-            try:
-                autonomousMovement('sad_script', 1.5, -0.2, 0.55)
-                autonomousMovement('sad_script', 1.5, -0.2, -0.6)
-            except rospy.ROSInterruptException:
-                pass
-        #Anger-Movement
-        elif emotion == "anger":
-            try:
-                autonomousMovement('anger_script', 5, 0.1, -1.0)
-            except rospy.ROSInterruptException:
-                pass
-        #Fear-Movement
-        elif emotion == "fear":
-            try:
-                autonomousMovement('fear_script', 2.5, -0.6, 0.4)
-                autonomousMovement('fear_script', 1.5, -0.7, -0.6)
-            except rospy.ROSInterruptException:
-                pass
-        #Confident-Movement
-        elif emotion == "confident":
-            try:
-                autonomousMovement('confident_script', 2, 0.3, 0.3)
-                autonomousMovement('confident_script', 1.25, 0.2, 0.9) 
-                autonomousMovement('confident_script', 1.25, 0.2, -0.9) 
-                autonomousMovement('confident_script', 1, 0.5, 0) 
-            except rospy.ROSInterruptException:
-                pass
-        #Happy-Movement
-        elif emotion == "happy":
-            try:
-                autonomousMovement('happy_script', 5, 0.4, -1.5) 
-            except rospy.ROSInterruptException:
-                pass
-        #Frustrated-Movement
-        elif emotion == "frustrated":
-            try:
-                autonomousMovement('frustrated_script', 5, 0.4, 1.5) 
-            except rospy.ROSInterruptException:
-                pass
-        #Tired-Movement
-        elif emotion == "tired":
-            try:
-                autonomousMovement('tired_script', 5, 0.1, 0) 
-            except rospy.ROSInterruptException:
-                pass
-        #Neutral
-        elif emotion == "neutral":
-            try:
-                autonomousMovement('neutral', 5, 0, 0) 
-            except rospy.ROSInterruptException:
-                pass
-            
-        elif emotion == "exit":
-            speak("Goodbye")
+        print("Available Emotions: ", bot.emotion.keys())
+        emotion = input("Enter an emotion: ").strip().lower()
+        if emotion == "exit":
             rospy.loginfo("Exiting...")
             sys.exit()
-        
-
+        bot.executeEmotion(emotion)
 
 
 if __name__ == "__main__":
@@ -133,6 +76,10 @@ if __name__ == "__main__":
         pass
 
 
+
+
+
+        
 
 
 if __name__ == "__main__":
---
+++ scripts/story_script.py
@@ -0,0 +1,6 @@
+from movement_script import Turtlebot
+from TTS_script import TextToSpeech
+
+import rospy
+import sys
+import rospy
